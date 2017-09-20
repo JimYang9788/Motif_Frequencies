c565029eb5175315bf83d9018b347c9944979c83
Jim Yang <jimyang@Jims-MacBook-Pro.local>
2017-09-20 09:43:31 -0400

    third commit

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ class Vertex:
         return self.connectedTo.keys()
 
 data_set = {}
-source  = open("sample.txt", "r")
+source  = open("facebook_combined.txt", "r")
 
 ID = ""
 Adjacent = ""
@@ -61,30 +61,53 @@ source.close()
 
 #print length
 def size_three_motif ():
+    total = 0
     open_triangle = 0
-    close_triangle = 0
+    closed_triangle = 0
     for i in range(0, 5000):
         random1 = random.randint(0, total_nodes - 1)
-
         #randomly pick one data point from the ID list generated
         n1_key = data_set.keys()[random1]
-        n1_node = data_set([n1_key])
+        n1_node = data_set[n1_key]
         n1_len = len(Vertex.getConnections(n1_node))
+
+        #Select a random2 to determine the second node
+        #Making sure that random 2 is different from random 1,
+        # So we don't repick the same node again.
         random2 = random.randint(0, n1_len - 1)
         while (random2 == random1):
             random2 = random.randint(0, n1_len - 1)
+
         n2_key = Vertex.getConnections(n1_node)[random2]
-        n2_node = data_set.values([n2_key])
+        n2_node = data_set[n2_key]
         n2_len = len(Vertex.getConnections(n2_node))
-        if (n2_len == 1):
-            open_triangle = open_triangle + 1
+        random3 = random.randint(0, n2_len - 1)
+
+        #Making sure that random 1,2,3 are different
+        while random3 == random1 or random3 == random2:
+            random3 = random.randint(0, n2_len + n1_len - 1)
+        n3_key = (Vertex.getConnections(n2_node)+Vertex.getConnections(n1_node))[random3]
+        n3_node = data_set[n3_key]
+        #print n1_key + " " + n2_key + " " + n3_key
+        total = total + 1
+
+        if n1_key in Vertex.getConnections(n3_node):
+            closed_triangle = closed_triangle + 1
         else:
-            random3 = random.randint(0, n2_len - 1)
-            while random3 == random1 or random3 == random2:
-                random3 = random.randint(0, n2_len - 1)
-        n3_key = Vertex.getConnections(n2_node)[random3]
-        n3_node = data_set.values([n3_key])
-        print n1_key + " " + n2_key + " " + n3_key
+            open_triangle = open_triangle + 1
+
+    print open_triangle
+    print closed_triangle
+
+    print total
+
 
 
 size_three_motif ()
+
+#3000 2000
+#2900 2000
+#2950 2050
+#2955 2045
+#2956 2944
+#2999 2001
